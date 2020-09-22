4dc9faf9a6811556aea0640017d92d00a12dc928
Aminabdelrehim <71644124+Aminabdelrehim@users.noreply.github.com>
2020-09-22 10:11:21 +0200

    Update GamePiece.java

--- GamePiece.java
+++ GamePiece.java
@@ -7,3 +7,31 @@ public class GamePiece {
 
 
 }
+public class GameObject {
+    public int x, y;
+ 
+    public GameObject(int x, int y) {              // Konstruktorn som deklarerar klassvariablerna
+        this.x = x;
+        this.y = y;
+    }
+ 
+    public int getX() {                            // Returnera respektive koordinat 
+        return x;
+    }
+ 
+    public int getY() {
+        return y;
+    }
+ 
+    public void setX(int x) {                      // Sätt ett nytt värde på x respektive y
+        this.x = x;
+    }
+ 
+    public void setY(int y) {
+        this.y = y;
+    }
+ 
+    public Rectangle getBounds(int x, int y){       //Returnerar det område som objektet täcker på spelplanen,
+        return new Rectangle(x, y, 64, 64);         //(så att man kan avgöra om två objekt kolliderar med varandra)
+ 
+}
