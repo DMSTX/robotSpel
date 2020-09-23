c0fe10ea95c3e5d258ca97d6704d02ca23fbf731
DMSTX <71629413+DMSTX@users.noreply.github.com>
2020-09-23 10:54:49 +0200

    Delete Zebra.java

--- Zebra.java
+++
@@ -1,8 +0,0 @@
-public class Zebra extends GamePiece { 
-  private double speedX = 0; // Hastigheten på spelaren i x- och y-led
-  private double speedY = 0;
-  
-  public Zebra (int x, int y) { //Konstruktor
-    super(x, y);
-  }
-  
